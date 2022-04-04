6077ad2ba695b907008d0582471b2fcfc32d127e
onyebuchi okofu <onyebuchiokofu@gmail.com>
2022-04-04 18:07:03 +0100

    heade and button added

---
+++ task/src/components/Button.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <button @click="onClick()" :style="{backgound:color}" class="btn" >{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonTask',
+  props: {
+    text: String,
+    color: String
+  },
+  methods: {
+    onClick () {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
---
+++ task/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>{{title }}</h1>
+      <Button  text= "add task" color= "green"/>
+  </header>
+
+</template>
+
+<script>
+import Button from '@/components/Button'
+
+export default {
+  name: 'HeaderTask',
+  props: {
+    title: String
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.btn {
+  display: inline-block;
+  background: rgb(79, 247, 101);
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+</style>
